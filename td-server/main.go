355661ea99af942c37be48fc687f35b289a8ed21
Bry Torres <bryan@framewerk.io>
2022-12-15 02:29:38 -0500

    Prep for heroku

--- td-server/main.go
+++ td-server/main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"os"
 	"td-talk-websocket/handler"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
-
 
 func main() {
 	router := gin.Default()
 	router.GET("/", handler.WebsocketHandler)
-	router.Run(":4242")
+
+	godotenv.Load("../.env")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "4242"
+	}
+
+	router.Run(":" + port)
 }
